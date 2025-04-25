a61a0e001b84ca49774a4e1078e76f2e8807cbda
Tazshelby2016 <tazshelby2016@gmail.com>
2025-04-25 02:17:03 +0000

    Broken

--- src/pages/PokemonCard.jsx
+++ src/pages/PokemonCard.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardContent,CardHeader, CardDescription, Icon, Image, Input, List, ListItem, Label} from 'semantic-ui-react'
+import { Card, CardContent,CardHeader, CardDescription, Icon, Image, Input, List, ListItem, ListContent, Label} from 'semantic-ui-react'
 import '../App.scss';
+import axios from 'axios'
 
-const SearchForm = ()=>{
+const SearchForm = ({poke, pokeSetter})=>{
   return (
   <>
-    <Input fluid
+    <Input id="Search" value={poke} onChange={val => pokeSetter(val.target.val)} fluid
     icon={<Icon name='search' inverted circular link />}
     placeholder='Search for Pokemon...'/>
   </>
@@ -14,39 +15,59 @@ const SearchForm = ()=>{
 
 
 const PokemonCard = ({pokemonID}) => {
+  const [data, setData] = useState(null);
+  const pokéurl = "https://pokeapi.co/api/v2/pokemon/" + pokemonID;
+  useEffect(() =>{
+    axios.get(pokéurl)
+    .then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch((error)=>{
+      console.error(error);
+      console.error(pokéurl)
+    })
+  }, [pokemonID]);
+  if (data === null){
+    console.error("Returned nothing.");
+    return;
+  }
+  if (data.types === null){
+    console.error("Returned no type.");
+    return;
+  }
+
+
+  const typeList = data.types.map((type) => <Label>{type.type.name}</Label>);
+  const statList = data.stats.map((stat) => <ListItem><ListContent floated='left'>{stat.stat.name}</ListContent><ListContent floated = "right">{stat.base_stat}</ListContent></ListItem>);
+
   return (
     <Card>
-      <Image src="Owl.png"/>
+      <Image src={data.sprites.front_default}/>
       <CardContent>
         <CardHeader>
-          MissingNo
+          {data.name}
         </CardHeader>
         <CardDescription>
-          <List divided>
-            <ListItem>
-              Hello
-            </ListItem>
-            <ListItem>
-              Hello
-            </ListItem>
-            <ListItem>
-              Hello
-            </ListItem>
+          <List divided size="large">
+            {statList}
           </List>
         </CardDescription>
       </CardContent>
       <CardContent Extra>
-      <Label>bird</Label>
+      {typeList}
       </CardContent>
     </Card>
     );
 }
 const PokemonCardPage = () => {
+  const [poke, setPoke] = useState(null);
   return (
     <div className="CenteredLayout">
       <div>
-        <SearchForm></SearchForm>
-        <PokemonCard></PokemonCard>
+        <SearchForm poke={poke} pokeSetter={setPoke}></SearchForm>
+        <p>{poke}</p>
+        <PokemonCard pokemonID="type-null"></PokemonCard>
       </div>
     </div>
   );
