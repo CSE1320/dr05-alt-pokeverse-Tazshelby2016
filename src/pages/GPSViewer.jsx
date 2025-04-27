5536a69ee55a51bb1bef176cadba505b2bb916c3
Tazshelby2016 <tazshelby2016@gmail.com>
2025-04-27 18:06:11 +0000

    Fix GPS Viewer, start clean up of Pokemon Card

--- src/pages/GPSViewer.jsx
+++ src/pages/GPSViewer.jsx
@@ -1,13 +1,62 @@
-
-
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { List, Segment, Button, Header, Label, Divider, Statistic} from 'semantic-ui-react';
 import '../App.scss';
+
+const GetCoord = (geoLoc=[], geoLocSetter, geoWatchPos) => {
+  geoLocSetter(geoLoc.concat(geoWatchPos));
+  return;
+}
+
+//Number Formatting
+const FormatNumber = (number=0) =>{
+  return parseFloat(number).toFixed(2);
+}
+
+const GPSLog = ({locList}=[]) => {
+  const noList = (<p> No logs </p>);
+  const list = locList.map((location, index) => <GPSLogItem lat={location.latitude} long={location.longitude} index={index} />);
+  const combList = (<List>{list}</List>);
+  return (locList.length > 0)?(combList):(noList);
+}
+
+const GPSLogItem = ({lat=0,long=0, index}) => {
+  return(
+    <List.Item key={index}>
+      <List.Content floated='right'>{FormatNumber(lat)}</List.Content>
+      <List.Content>Latitude</List.Content>
+      <List.Content floated='right'>{FormatNumber(long)}</List.Content>
+      <List.Content>Longitude</List.Content>
+    </List.Item>    
+  );
+}
 
 const GPSViewer = () => {
   const student = "Bryan Badgero";
 
+  // Stores all locations
+  const [geoLocs, setGeoLocs] = useState([]);
+  const [currentLocs, setCurrentLocs] = useState([]);
+
+
+
+
+  useEffect(
+    () =>{
+      if("geolocation" in navigator)
+      {
+        const geoSuccess = (position) => {
+          setCurrentLocs(position.coords);
+          return;
+        }
+        const geoOptions = {
+          highAccuracy: false,
+          lifetime: 30000,
+          timeout: 3000,
+        }
+        const WatchID = (navigator.geolocation.watchPosition(geoSuccess, null, geoOptions));
+      }
+    }, [setCurrentLocs]
+  );
   // TODO
 
   return (
@@ -16,12 +65,12 @@ const GPSViewer = () => {
         <div className="VerticalColumn">
         <Header as="h2">{student}</Header>
           <Statistic>
-            <Statistic.Value>XXX</Statistic.Value>
+            <Statistic.Value>{FormatNumber(currentLocs.latitude)}</Statistic.Value>
             <Statistic.Label>Latitude</Statistic.Label>
           </Statistic>
           <br/>
           <Statistic>
-            <Statistic.Value>XXX</Statistic.Value>
+            <Statistic.Value>{FormatNumber(currentLocs.longitude)}</Statistic.Value>
             <Statistic.Label>Longitude</Statistic.Label>
           </Statistic>
         </div>
@@ -29,20 +78,13 @@ const GPSViewer = () => {
 
 
         <Header as="h3" dividing>Logged Points</Header>
-        <p> No logs </p>
-        <List>
-          <List.Item>
-              <List.Content floated='right'>XXX</List.Content>
-              <List.Content>Latitude</List.Content>
-              <List.Content floated='right'>XXX</List.Content>
-              <List.Content>Longitude</List.Content>
-          </List.Item>
-        </List>
+        
+        <GPSLog locList={geoLocs}/>
 
 
         
         <Divider></Divider>
-        <Button primary fluid> 
+        <Button primary fluid onClick={()=>GetCoord(geoLocs, setGeoLocs, currentLocs)}> 
           <span className="badge-button">LOG</span>
           <Label circular color='blue' as='a'>0</Label>
         </Button>
--- src/pages/PokemonCard.jsx
+++ src/pages/PokemonCard.jsx
@@ -3,21 +3,9 @@ import { Card, CardContent,CardHeader, CardDescription, Icon, Image, Input, List
 import '../App.scss';
 import axios from 'axios'
 
-const SearchForm = ({poke, pokeSetter})=>{
+const SearchForm = ({pokeSetter})=>{
   const [searchVal, setSearch] = useState("");
 
-  let tempVal = "";
-
-  const checkSearchVal = (val,prevVal, setVal) => {
-    if (val !== prevVal) {
-      setVal(val);
-      console.log("Set to", val)
-    } 
-    else{
-      console.error("Trying to replace existing value")
-    }
-
-  };
   return (
   <>
     <Input id="inptValue" value={searchVal} onChange={val => setSearch(val.target.value)} fluid
@@ -31,17 +19,23 @@ const SearchForm = ({poke, pokeSetter})=>{
 const PokemonCard = ({pokemonID}) => {
   const [data, setData] = useState(null);
   const pokéurl = "https://pokeapi.co/api/v2/pokemon/" + pokemonID;
-  useEffect(() =>{
-    axios.get(pokéurl)
-    .then((response) => {
-      console.log(response.data);
-      setData(response.data);
-    })
-    .catch((error)=>{
-      console.error(error);
-      console.error(pokéurl)
-    })
-  }, [pokemonID]);
+  useEffect(
+    () =>{
+      axios.get(pokéurl)
+      .then(
+        (response) => {
+          console.log(response.data);
+          setData(response.data);
+        }
+      )
+      .catch(
+        (error)=>{
+          console.error(error);
+          console.error(pokéurl)
+        }
+      )
+    }, [pokéurl, pokemonID]
+  );
   if (data === null){
     console.error("Returned nothing.");
     // This is bad, but I do not remember how to fix.
@@ -52,8 +46,8 @@ const PokemonCard = ({pokemonID}) => {
     // This is bad, but I do not remember how to fix.
     return(<Card><CardHeader>Failed To Retrieve Pokemon.</CardHeader></Card>);
   }
-  const typeList = data.types.map((type) => <Label>{type.type.name}</Label>);
-  const statList = data.stats.map((stat) => <ListItem><ListContent floated='left'>{stat.stat.name}</ListContent><ListContent floated = "right">{stat.base_stat}</ListContent></ListItem>);
+  const typeList = data.types.map((type, index) => <Label key={index}>{type.type.name}</Label>);
+  const statList = data.stats.map((stat, index) => <ListItem key={index}><ListContent floated='left'>{stat.stat.name}</ListContent><ListContent floated = "right">{stat.base_stat}</ListContent></ListItem>);
 
   return (
     <Card>
@@ -79,7 +73,7 @@ const PokemonCardPage = () => {
   return (
     <div className="CenteredLayout">
       <div>
-        <SearchForm poke={poke} pokeSetter={setPoke}></SearchForm>
+        <SearchForm pokeSetter={setPoke}></SearchForm>
         <PokemonCard pokemonID={poke}></PokemonCard>
       </div>
     </div>
